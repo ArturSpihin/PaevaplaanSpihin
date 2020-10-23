5c2ad386939b58e8e6d91f1da0e23333e1908c31
opilane <opilane@HP-CZC6297B8L>
2020-10-23 12:01:36 +0300

    Add project files.

---
+++ PaevaplaanSpihin/PaevaplaanSpihin/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PaevaplaanSpihin
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            StackLayout st = new StackLayout();
+            Button esm_btn = new Button { Text = "Esmaspäev" };
+            st.Children.Add(esm_btn);
+            esm_btn.Clicked += new EventHandler(Button_ClickedAsync);
+            Content = st;
+        }
+
+        private async Task Button_ClickedAsync(object sender, EventArgs e)
+        {
+            var btn = (Button)sender;
+            switch (btn.Text)
+            {
+                case "Esmaspäev":
+                    await Navigation.PopAsync();
+                    Esmaspaev esmaspaev = new Esmaspaev();
+                    await Navigation.PopAsync(esmaspaev);
+                    break;
+            }
+
+
+
+        }
+    }
+}
